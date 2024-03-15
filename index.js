9b29b76a6de44984c679aa33d736b1ee5ae71b78
William Ndirangu <wwagura0@gmail.com>
2024-03-15 03:10:51 +0300

    Adds objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam"
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }});
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value){
+    return {...employee, [streetAddress]: "11 Broadway"};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value){
+    employee[streetAddress] = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee};
+    delete newEmployee[name];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name];
+    return employee;
+}
